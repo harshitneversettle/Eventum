73c2c01fa44bfa4536825365217cdd33dc55ff14
Harshit yadav <harshityadav5499@gmail.com>
2025-12-21 00:21:38 +0530

    fix : resolve market

--- programs/eventum/src/instructions/resolve_market.rs
+++ programs/eventum/src/instructions/resolve_market.rs
@@ -1,5 +1,5 @@
 use anchor_lang::{
-    accounts::{signer, unchecked_account}, prelude::*, solana_program::native_token::LAMPORTS_PER_SOL, system_program // for LAMPORTS_PER_SOL
+    accounts::{self, signer, unchecked_account}, prelude::*
 };
 use anchor_spl::{
     associated_token::AssociatedToken,
@@ -14,26 +14,27 @@ use crate::ErrorCode ;
 pub struct ResolveMarket<'info>{
     
     /// CHECK: only for deriving the seeds 
-    pub creater : UncheckedAccount<'info> ,
+    pub creator : UncheckedAccount<'info> ,
 
     #[account(
         mut ,
-        seeds =[ b"Market" , creater.key().as_ref() , &unique_market_id.to_le_bytes()] ,
+        seeds =[ b"Market" , creator.key().as_ref() , &unique_market_id.to_le_bytes()] ,
         bump
     )]
     pub market : Account<'info , Market>,
 
     #[account(mut)]
     pub oracle_authority : Signer<'info> ,
-    
-
 }
 
 pub fn handler(ctx : Context<ResolveMarket> , unique_market_id : u64 , outcome : bool )->Result<()>{
     let market = &mut ctx.accounts.market ;
-    let creater = ctx.accounts.creater.key() ;
+    let creator = ctx.accounts.creator.key() ;
+    require!(market.creator == creator, ErrorCode::InvalidCreator);
+    //require!(Clock::get()?.unix_timestamp >= market.end_time , ErrorCode::MarketNotExpired) ;
+    let creator = ctx.accounts.creator.key() ;
     let oracle_auth = ctx.accounts.oracle_authority.key() ;
-    require!(market.resolved == false , ErrorCode::MarketResolved) ;
+    require!(!market.resolved , ErrorCode::MarketResolved) ;
     require!(market.oracle_authority == oracle_auth , ErrorCode::OracleNotMatched ) ;
 
     market.resolved = true ;
